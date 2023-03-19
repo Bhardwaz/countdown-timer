a2770f21a90d7b394f4b10732bdf762361763c14
sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2023-03-20 00:25:16 +0530

    code updated of script

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
- const countdownEl = document.querySelector('.countdown-container')
- const outputMinutes = document.querySelector('.minutes')
- const outputSeconds = document.querySelector('.seconds')
+const countdownEl = document.querySelector('.countdown-container')
+const outputMinutes = document.querySelector('.minutes')
+const outputSeconds = document.querySelector('.seconds')
 
-let [interval, time, minutes, seconds] = [null, 63, 0, 0]
-const calculateTiming = time => {
- minutes = Math.floor(time/60)
- seconds = time - (minutes * 60)
- if(minutes < 1 )minutes = 0
- if(seconds < 0 )seconds = 59
- 
- outputMinutes.textContent = minutes
- outputSeconds.textContent = seconds
+let [interval, totalSeconds, minutes, seconds] = [null, 0, 0, 0]
+const enteredTime = prompt("Please enter the minutes and seconds with a space")
 
- if(minutes === 0 && seconds === 0){
-    clearInterval(interval)
-    countdownEl.classList.add("timeup")
-    countdownEl.textContent = "Time Up"
- }
+const calculateSeconds = () => {
+    const [enteredMinutes, enteredSeconds] = enteredTime.split(' ')
+    totalSeconds = Number(enteredMinutes) * 60 + Number(enteredSeconds)
 }
-// set the time in seconds  
-interval = setInterval(function(){
-calculateTiming(time)
-time--
+calculateSeconds()
+const displayTiming = totalSeconds => {
+    minutes = Math.floor(totalSeconds / 60)
+    seconds = totalSeconds % 60
+    if (minutes < 1) minutes = 0
+    if (seconds < 0) seconds = 59
+    outputMinutes.textContent = minutes
+    outputSeconds.textContent = seconds
+
+    if (minutes === 0 && seconds === 0) {
+        clearInterval(interval)
+        countdownEl.classList.add("timeup")
+        countdownEl.textContent = "Time Up"
+    }
+}
+
+// set the time in seconds
+interval = setInterval(() => {
+    displayTiming(totalSeconds)
+    totalSeconds--
 }, 1000)
-
-
-
- 
-
-
